a48fe2dfede8841e2ebc128bf43b189ac1fdd7b8
gianfillipo <gian.angelo@sptech.school>
2023-04-11 02:58:12 +0000

    Adding data.sql to insert data in the database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,17 @@
+INSERT INTO instrument (name) VALUES
+('Guitarra'),
+('Baixo'),
+('Bateria'),
+('Teclado'),
+('Piano'),
+('Violino'),
+('Trompete'),
+('Saxofone'),
+('Clarinete'),
+('Flauta'),
+('Violão'),
+('Cavaquinho'),
+('Pandeiro'),
+('Surdo'),
+('Tamborim');
+
