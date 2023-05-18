1e4bd29f1b3188d45714404a63c9b6d3e95e7aa7
gianfillipo <gian.angelo@sptech.school>
2023-05-18 19:29:08 +0000

    Correcting native query to findEventsByLocation

--- src/main/kotlin/com/example/authenticationservice/dao/EventRepository.kt
+++ src/main/kotlin/com/example/authenticationservice/dao/EventRepository.kt
@@ -5,13 +5,20 @@ import com.example.authenticationservice.model.Event
 import com.example.authenticationservice.model.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import java.time.LocalDate
 
 interface EventRepository : JpaRepository<Event, Long> {
     abstract fun findByIdAndUserAndFinalized(id: Long, user: User, finalized: Boolean): Event?
   //    @Query("SELECT ej FROM EventJob ej JOIN ej.instrument i WHERE i.id IN :instrumentIds AND ej.event.finalized = false AND ej.event.eventDate >= :currentDate")
-    @Query("SELECT e FROM Event e WHERE e.finalized = false AND e.eventDate >= :currentDate")
-    fun findUnfinalizedEventsAfterOrEqual(currentDate: LocalDate): List<Event>
+  @Query(value = """
+    SELECT * FROM event_job
+    JOIN event
+    ON event_job.event_id = event.id
+    WHERE event.finalized = false AND event.event_date >= :currentDate
+    AND event_job.instrument_id IN :instrumentIds
+    """, nativeQuery = true)
+  fun findUnfinalizedEventsAfterOrEqual(@Param("currentDate") currentDate: LocalDate, @Param("instrumentIds") instrumentsId: List<Long>): List<Event>
     fun existsByEventDateAndFinalized(eventDate: LocalDate, b: Boolean): Boolean
     @Query("SELECT count(e) > 0 FROM Event e WHERE e.id = :id AND e.user.id = :userId AND e.finalized = false")
     fun existsByIdAndUserIdAndFinalizedFalse(id: Long, userId: Long): Boolean
